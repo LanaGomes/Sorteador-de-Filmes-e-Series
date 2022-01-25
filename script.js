13e98f8469d78d1b2e70152e60dec9d3736c938c
LanaGomes <lana.gomes1919@gmail.com>
2022-01-25 20:17:25 -0300

    Trying to make addMovieButton work

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const arrayFilms = ['Eternos','Encanto','Bios','Infinito','O último duelo']
+let itensList;
+const buttonAddMovie = document.getElementById('button_addMovie');
+button_addMovie.classList.remove("active");
 
 MovieList();
 
 let buttonSortear = document.getElementById('button_Sortear');
 buttonSortear.addEventListener('click',chooseRandonMovie);
+
+button_addMovie.addEventListener('click',addMovie);
 
 Array.prototype.getRandom = function(){
     return this[Math.floor(Math.random()*this.length)];
@@ -17,11 +22,24 @@ function chooseRandonMovie (){
 
 /*como interligar lista de array existente a lista não ordenada*/
 function MovieList() {
-    for (var i = 0; i < arrayFilms.length; i++) {
+    if(addMovie.className == "active"){
+      arrayFilms.length = 0;
+      console.log(arrayFilms);
+    } 
+    
+      for (var i = 0; i < arrayFilms.length; i++) {
       const itensList = document.getElementById('availableMoviesList'); 
-      const newMovie = document.createElement('li');
+      let newMovie = document.createElement('li');
       newMovie.innerHTML = arrayFilms[i];
       itensList.appendChild(newMovie); 
   }
+}
+
+function addMovie(){
+  let newMovie = window.prompt("Adicione seu filme,então!");
+  arrayFilms.push(newMovie);
+  console.log(arrayFilms);
+  button_addMovie.className += " active";
+  MovieList();
 }
  
